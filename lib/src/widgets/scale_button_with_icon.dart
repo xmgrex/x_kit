7266d750385c9dbb0bcd1f89dc3a3e8bbeed822d
kyouyagenki <rebertas.g@ezweb.ne.jp>
2022-05-21 19:28:01 +0900

    T

--- lib/src/widgets/scale_button_with_icon.dart
+++ lib/src/widgets/scale_button_with_icon.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_scale_tap/flutter_scale_tap.dart';
 
-
 class ScaleButtonWithIcon extends StatelessWidget {
   const ScaleButtonWithIcon({
     Key? key,
@@ -39,7 +38,8 @@ class ScaleButtonWithIcon extends StatelessWidget {
         width: width ?? 142,
         curve: Curves.easeOutCirc,
         duration: const Duration(milliseconds: 400),
-        foregroundDecoration: BoxDecoration(
+        foregroundDecoration: const BoxDecoration(color: Colors.transparent),
+        decoration: BoxDecoration(
             color: buttonColor ?? Theme.of(context).primaryColor,
             borderRadius: BorderRadius.circular(radius ?? 60)),
         child: Row(
