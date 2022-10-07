cab456d4a867bb9d66b01374a58b9649051bbdd1
kyouyagenki <rebertas.g@ezweb.ne.jp>
2022-10-07 14:12:10 +0900

    edit dependency

--- lib/src/utils/symbol.dart
+++ lib/src/utils/symbol.dart
@@ -1,7 +1,5 @@
 import 'package:currency_formatter/currency_formatter.dart';
 
-final CurrencyFormatter cf = CurrencyFormatter();
-
 String symbolFormatter(dynamic amount, String currency) {
-  return cf.format(amount, cf.majors[currency.toLowerCase()]!);
+  return CurrencyFormatter.format(amount, CurrencyFormatter.majors[currency.toLowerCase()]!);
 }
