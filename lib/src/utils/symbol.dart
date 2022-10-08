41c8ec67ed42e5829a260e0333bbfaf2f8d4cb4d
kyouyagenki <rebertas.g@ezweb.ne.jp>
2022-10-08 21:30:15 +0900

    c

--- lib/src/utils/symbol.dart
+++ lib/src/utils/symbol.dart
@@ -2,9 +2,11 @@ import 'package:currency_formatter/currency_formatter.dart';
 
 String symbolFormatter(dynamic amount, {String? currency}) {
   final c = CurrencyFormatterSettings.local;
+  final formatter = currency != null
+      ? CurrencyFormatter.majors[currency.toLowerCase()]!
+      : c ?? CurrencyFormatterSettings.jpy;
   return CurrencyFormatter.format(
     amount,
-    CurrencyFormatter
-        .majors[currency != null ? currency.toLowerCase : c!.symbol]!,
+    formatter,
   );
 }
