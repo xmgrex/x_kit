1be14fa78ced8bc1960060500576c7e1ade87557
kyouyagenki <rebertas.g@ezweb.ne.jp>
2023-01-27 15:30:00 +0900

    package upgrade

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import dynamic_color
 import macos_ui
-import path_provider_macos
+import path_provider_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
