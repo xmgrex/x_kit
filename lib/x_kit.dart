01774bead6382de78842ff95d12092dae4acb47d
kyouyagenki <rebertas.g@ezweb.ne.jp>
2023-12-15 18:00:31 +0900

    Reorder package imports for better readability

--- lib/x_kit.dart
+++ lib/x_kit.dart
@@ -1,21 +1,21 @@
 library x_kit;
 
-export 'src/router/router.dart';
-export 'src/utils/utils.dart';
-export 'src/widgets/widgets.dart';
-export 'src/riverpod/riverpod.dart';
-export 'src/styles/styles.dart';
-export 'src/extensions/extensions.dart';
-export 'src/converter/converter.dart';
-
 // export 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 export 'package:cached_network_image/cached_network_image.dart';
+export 'package:expandable/expandable.dart';
 export 'package:fluentui_icons/fluentui_icons.dart';
 export 'package:fluentui_system_icons/fluentui_system_icons.dart';
 export 'package:flutter_font_icons/flutter_font_icons.dart';
-export 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
-export 'package:flutter_scale_tap/flutter_scale_tap.dart';
-export 'package:expandable/expandable.dart';
 export 'package:flutter_iconly/flutter_iconly.dart';
-export 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+export 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
 export 'package:flutter_riverpod/flutter_riverpod.dart';
+export 'package:flutter_scale_tap/flutter_scale_tap.dart';
+export 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+export 'src/converter/converter.dart';
+export 'src/extensions/extensions.dart';
+export 'src/riverpod/riverpod.dart';
+export 'src/router/router.dart';
+export 'src/styles/styles.dart';
+export 'src/utils/utils.dart';
+export 'src/widgets/widgets.dart';
