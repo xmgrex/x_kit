c5492b0956e0b49b9d8031f212e83044a7babac8
kyouyagenki <rebertas.g@ezweb.ne.jp>
2023-12-15 18:19:37 +0900

    Remove snack_bar utility function

--- lib/src/utils/snack_bar.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSnackBar(
-    BuildContext context, Widget? content) {
-  return ScaffoldMessenger.of(context).showSnackBar(
-    content is SnackBar ? content : SnackBar(content: content!),
-  );
-}
--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,4 +1,3 @@
 export 'symbol.dart';
-export 'snack_bar.dart';
 export 'logger.dart';
 export 'gaps.dart';
