3dd0318230272f0578d0e866c3a9bea4ae3287f0
kyouyagenki <rebertas.g@ezweb.ne.jp>
2023-12-15 19:55:16 +0900

    Reorder extensions imports and add list and string extensions

--- lib/src/extensions/extensions.dart
+++ lib/src/extensions/extensions.dart
@@ -1,13 +1,10 @@
 library extensions;
 
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:x_kit/x_kit.dart';
 
 part './context_extension.dart';
-part './string_extension.dart';
-part './list_extension.dart';
 part './date_time_extension.dart';
+part './list_extension.dart';
+part './string_extension.dart';
 part '../extensions/text_style_extension.dart';
