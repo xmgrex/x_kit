12d1940ae26867caa8fc305a0bb99887a9bbf71a
kyouyagenki <rebertas.g@ezweb.ne.jp>
2023-12-17 18:11:46 +0900

    Update dependencies to latest versionsReasoning:- Upgrading dependencies to their latest versions ensures that the codebase is using the most recent functionality and bug fixes provided by the libraries

--- lib/x_kit.dart
+++ lib/x_kit.dart
@@ -12,6 +12,7 @@ export 'package:flutter_riverpod/flutter_riverpod.dart';
 export 'package:flutter_scale_tap/flutter_scale_tap.dart';
 export 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 export 'package:animate_do/animate_do.dart';
+export 'package:adaptive_dialog/adaptive_dialog.dart';
 
 export 'src/converter/converter.dart';
 export 'src/extensions/extensions.dart';
