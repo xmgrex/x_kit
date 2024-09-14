1d14450e7b5dd8c1544adaaf4657d0330c05756d
kyouyagenki <rebertas.g@ezweb.ne.jp>
2024-09-14 14:05:46 +0900

    chore: Refactor CupertinoTextStyles to use predefined text styles

--- lib/src/styles/cupertino_text_styles.dart
+++ lib/src/styles/cupertino_text_styles.dart
@@ -96,74 +96,86 @@ class CupertinoTextStyles {
 
 }
 
-//// Wrapper for TextStyle
+/// Wrapper for [TextStyle] that provides additional predefined text styles.
 class _CupertinoMyTextStyle extends TextStyle {
   _CupertinoMyTextStyle(TextStyle style)
-      : super(
-          color: style.color,
-          backgroundColor: style.backgroundColor,
-          fontSize: style.fontSize,
-          fontWeight: style.fontWeight,
-          fontStyle: style.fontStyle,
-          letterSpacing: style.letterSpacing,
-          wordSpacing: style.wordSpacing,
-          textBaseline: style.textBaseline,
-          height: style.height,
-          leadingDistribution: style.leadingDistribution,
-          locale: style.locale,
-          foreground: style.foreground,
-          background: style.background,
-          shadows: style.shadows,
-          fontFeatures: style.fontFeatures,
-          decoration: style.decoration,
-          decorationColor: style.decorationColor,
-          decorationStyle: style.decorationStyle,
-          decorationThickness: style.decorationThickness,
-          debugLabel: style.debugLabel,
-          overflow: style.overflow,
-        );
+    : super(
+      color: style.color,
+      backgroundColor: style.backgroundColor,
+      fontSize: style.fontSize,
+      fontWeight: style.fontWeight,
+      fontStyle: style.fontStyle,
+      letterSpacing: style.letterSpacing,
+      wordSpacing: style.wordSpacing,
+      textBaseline: style.textBaseline,
+      height: style.height,
+      leadingDistribution: style.leadingDistribution,
+      locale: style.locale,
+      foreground: style.foreground,
+      background: style.background,
+      shadows: style.shadows,
+      fontFeatures: style.fontFeatures,
+      decoration: style.decoration,
+      decorationColor: style.decorationColor,
+      decorationStyle: style.decorationStyle,
+      decorationThickness: style.decorationThickness,
+      debugLabel: style.debugLabel,
+      overflow: style.overflow,
+    );
 
+  /// Predefined text style with black color.
   _MyTextStyle get black =>
-      _MyTextStyle(copyWith(color: const Color(0xFF000000)));
+    _MyTextStyle(copyWith(color: const Color(0xFF000000)));
 
+  /// Predefined text style with white color.
   _MyTextStyle get white =>
-      _MyTextStyle(copyWith(color: const Color(0xFFFFFFFF)));
+    _MyTextStyle(copyWith(color: const Color(0xFFFFFFFF)));
 
+  /// Predefined text style with grey color.
   _MyTextStyle get grey =>
-      _MyTextStyle(copyWith(color: const Color(0xFF8E8E93)));
+    _MyTextStyle(copyWith(color: const Color(0xFF8E8E93)));
 
+  /// Predefined text style with success color.
   _MyTextStyle get success =>
-      _MyTextStyle(copyWith(color: const Color(0xFF34C759)));
+    _MyTextStyle(copyWith(color: const Color(0xFF34C759)));
 
+  /// Predefined text style with error color.
   _MyTextStyle get error =>
-      _MyTextStyle(copyWith(color: const Color(0xFFFF3B30)));
+    _MyTextStyle(copyWith(color: const Color(0xFFFF3B30)));
 
+  /// Predefined text style with warning color.
   _MyTextStyle get warning =>
-      _MyTextStyle(copyWith(color: const Color(0xFFFF9500)));
+    _MyTextStyle(copyWith(color: const Color(0xFFFF9500)));
 
+  /// Predefined text style with link color.
   _MyTextStyle get link =>
-      _MyTextStyle(copyWith(color: const Color(0xFF0A84FF)));
+    _MyTextStyle(copyWith(color: const Color(0xFF0A84FF)));
 
+  /// Predefined text style with underline decoration.
   _MyTextStyle get underline =>
-      _MyTextStyle(copyWith(decoration: TextDecoration.underline));
+    _MyTextStyle(copyWith(decoration: TextDecoration.underline));
 
+  /// Predefined text style with line-through decoration.
   _MyTextStyle get lineThrough =>
-      _MyTextStyle(copyWith(decoration: TextDecoration.lineThrough));
+    _MyTextStyle(copyWith(decoration: TextDecoration.lineThrough));
 
+  /// Predefined text style with overline decoration.
   _MyTextStyle get overline =>
-      _MyTextStyle(copyWith(decoration: TextDecoration.overline));
+    _MyTextStyle(copyWith(decoration: TextDecoration.overline));
 
   ////* Colors can be set
   ////
+  /// Returns a new text style with the specified [color].
   _MyTextStyle setColor(Color color) => _MyTextStyle(copyWith(color: color));
 }
 
+/// Wrapper for [_MyTextStyle] that provides a set of text styles.
 class _CupertinoMyTextStyleSet extends _MyTextStyle {
-  /// regular is this
+  /// The bold text style.
   @override
   final _MyTextStyle bold;
 
   _CupertinoMyTextStyleSet({required TextStyle regular, required TextStyle bold})
-      : bold = _MyTextStyle(bold),
-        super(regular);
+    : bold = _MyTextStyle(bold),
+    super(regular);
 }
